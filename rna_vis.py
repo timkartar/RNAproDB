3ab677c0e9f07ad14a3856abd4541bf9634f586d
timkartar <timkartar7879@gmail.com>
2023-10-10 09:51:54 -0700

    dssr running on full cleaned structure now, but code breaks downstream. See TODO in rna_vis

--- rna_vis.py
+++ rna_vis.py
@@ -1,9 +1,7 @@
 from Bio.PDB import MMCIFParser
 from structure_data import StructureData
 from split_entities import splitEntities
-from clean_rna import cleanRNA
 import os, sys
-from run_dssr import runDSSR
 from d3blocks import D3Blocks
 import pandas as pd
 import json
@@ -13,22 +11,30 @@ from run_dssr import runDSSR
 from get_edges import getEdges
 from process_graph import processEdges, processNodes
 from utilities import node_to_text
+import json
 
 parser = MMCIFParser()
 
 #update: no need to change anymore
 home =  os.path.dirname(os.path.abspath(__file__)) #change this line only 
 
-pdb_path = "{}/pdb/".format(home)
+pdb_path = "{}/dssr_output/".format(home)
 # pdb_file = "8fvi-assembly1.cif"
 prefix = '1ivs'
-pdb_file = "{}-assembly1.cif".format(prefix)
-original_structure = StructureData(os.path.join(pdb_path, pdb_file), name="co_crystal")
-protein, rna = splitEntities(original_structure) # split RNA and protein from structure
+pdb_file = "{}.tmp.cif".format(prefix)
+structure = StructureData(os.path.join(pdb_path, pdb_file), name="co_crystal")
+protein, rna = splitEntities(structure) # split RNA and protein from structure
 
-# NOTE: here we save the cleaned temporary file before processing it in runDSSR. For larger files, this poses a challenge.
-rna = cleanRNA(rna)
-data = runDSSR(rna, quiet=True, prefix='1ivs')
+#   I updated the code to move running dssr to run_dssr and I run it now together on 
+#   protein and RNA, this probably results in some changes in the output which breaks code
+#   downstream in this file. 
+#   TODO: uncomment the two lines below this comment, comment the runDSSR line and fix the 
+#   breakage that happens then. Remove runDSSR import when it's done.
+
+#with open("{}/{}-dssr.json".format(pdb_path, prefix)) as FH:
+#    data = json.load(FH, object_pairs_hook=collections.OrderedDict) #TODO: uncomment this
+
+data = runDSSR(rna, quiet=True, prefix='1ivs') #TODO: comment this out
 
 protein_interactions = getInteractions(protein, rna, prefix)
 pairs,backbone_edges, interaction_edges,stacks = getEdges(data, protein_interactions)
@@ -42,6 +48,7 @@ d3.D3graph.set_edge_properties(directed=True, marker_color='red') # setting earl
 
 # can probably pre-compute, then add to the dataframe and use that?
 # iterate through nodes to change colors, label, etc.
+
 d3.D3graph.node_properties = processNodes(d3.D3graph.node_properties)
 d3.D3graph.edge_properties = processEdges(d3.D3graph.edge_properties, backbone_edges)
 
--- run_dssr.py
+++ run_dssr.py
@@ -2,6 +2,11 @@ import subprocess
 import json
 import os, sys
 import collections
+from Bio.PDB import MMCIFParser
+from structure_data import StructureData
+from split_entities import splitEntities
+from clean_rna import cleanRNA
+import os, sys
 
 def runDSSR(structure, quiet=True, prefix='rna'):
     """Run DSSR on given PDB file.
@@ -13,7 +18,7 @@ def runDSSR(structure, quiet=True, prefix='rna'):
     """
     
     if not isinstance(structure, str):
-        file_name = "./dssr_output/{}.tmp.mmcif".format(prefix)
+        file_name = "./dssr_output/{}.tmp.cif".format(prefix)
         structure.save(file_name)
     else:
         file_name = structure
@@ -35,9 +40,32 @@ def runDSSR(structure, quiet=True, prefix='rna'):
     return DSSR
 
 if __name__ == "__main__":
+    '''
     fname = sys.argv[1]
     cifdir = "../data/cifs/"
+    parser = MMCIFParser()
+    '''
+    #update: no need to change anymore
+    home =  os.path.dirname(os.path.abspath(__file__)) #change this line only 
+    
+    pdb_path = "{}/pdb/".format(home)
+    # pdb_file = "8fvi-assembly1.cif"
+    prefix = '1ivs'
+    pdb_file = "{}-assembly1.cif".format(prefix)
+    original_structure = StructureData(os.path.join(pdb_path, pdb_file), name="co_crystal")
+    protein, rna = splitEntities(original_structure) # split RNA and protein from structure
+
+    # NOTE: here we save the cleaned temporary file before processing it in runDSSR. For larger files, this poses a challenge.
+    rna = cleanRNA(rna)
+
+    '''
     for item in open(fname,"r").readlines():
         item = item.strip()
         f = os.path.join(cifdir, fname)
+    '''
+    full = rna[0]
+    for chain in protein.get_chains():
+        full.add(chain)
+    
+    data = runDSSR(rna, quiet=True, prefix='1ivs')
 
