d1e010fa798da9b9ab1b40c7ba76619a47b3f59a
timkartar <timkartar7879@gmail.com>
2023-10-11 13:38:35 -0700

    log and ignore fails

--- run_dssr.sh
+++ run_dssr.sh
@@ -1,11 +1,11 @@
 #/bin/bash
 split -l 100 <(ls ../data/cifs/) ./txts/cifs- --numeric-suffixes
-for FILE in /home/raktim/rnaprodb/rnaprodb/txts/*;
+for FILE in /home/raktim/rnaprodb/rnaprodb/txts/$1*;
 do  
     f="$(basename -- $FILE)"
-    mkdir ./run/${f}_run
+    #mkdir ./run/${f}_run
     cp *.py ./run/${f}_run/
-    cp -r _data ./run/${f}_run/
+    #cp -r _data ./run/${f}_run/
     cd ./run/${f}_run
     python run_dssr.py $FILE &
     cd /home/raktim/rnaprodb/rnaprodb/
