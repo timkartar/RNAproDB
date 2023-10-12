c9130bea5b1414f6b368d87b847c4d89954ec559
Ari Cohen <aricohen@yahoo.com>
2023-10-12 12:40:17 -0700

    updated to work with d3graph, instead of d3blocks

--- rna_vis.py
+++ rna_vis.py
@@ -2,7 +2,7 @@ from Bio.PDB import MMCIFParser
 from structure_data import StructureData
 from split_entities import splitEntities
 import os, sys
-from d3blocks import D3Blocks
+from d3graph import d3graph, vec2adjmat
 import pandas as pd
 import json
 import collections
@@ -38,22 +38,34 @@ with open("{}/{}-dssr.json".format(pdb_path, prefix)) as FH:
 protein_interactions,ss_dict = getInteractions(protein, rna, prefix)
 pairs,backbone_edges, interaction_edges,stacks = getEdges(data, protein_interactions, ss_dict)
 all_edges = pairs + backbone_edges + interaction_edges + stacks
+# all_nodes = set()
+# for tuple in all_edges:
+#    if tuple[0] not in all_nodes:
+#       all_nodes.add(tuple[0])
+#    if tuple[1] not in all_nodes:
+#       all_nodes.add(tuple[1])
 
+# print(all_nodes)
+# df_nodes = pd.DataFrame(all_nodes, columns=['id'])
+# df_nodes_json = df_nodes.to_json(orient='records')
+# print(df_nodes_json)
 
+# df_links = pd.DataFrame(all_edges, columns=['source', 'target'])
+# df_links['weight'] = [20]*len(pairs) + [100]*(len(backbone_edges)) + [5]*(len(interaction_edges)) + [20]*(len(stacks))
+# df_links_json = df_links.to_json(orient='records')
+# print(df_links_json)
+d3 = d3graph()
 df = pd.DataFrame(all_edges, columns=['source', 'target'])
 df['weight'] = [20]*len(pairs) + [100]*(len(backbone_edges)) + [5]*(len(interaction_edges)) + [20]*(len(stacks))
-d3 = D3Blocks()
-d3.d3graph(df, filepath=None, showfig=False)
-d3.D3graph.set_edge_properties(directed=True, marker_color='red') # setting earlier to then update?
+adjmat = vec2adjmat(df['source'], df['target'], weight=df['weight'])
+d3.graph(adjmat)
+d3.set_edge_properties(directed=True, marker_color='red') # setting earlier to then update?
 
 
 # can probably pre-compute, then add to the dataframe and use that?
 # iterate through nodes to change colors, label, etc.
 
-d3.D3graph.node_properties = processNodes(d3.D3graph.node_properties)
-d3.D3graph.edge_properties = processEdges(d3.D3graph.edge_properties, backbone_edges, stacks)
+d3.node_properties = processNodes(d3.node_properties)
+d3.edge_properties = processEdges(d3.edge_properties, backbone_edges, stacks)
 
-d3.D3graph.show(filepath='{}/output/{}.html'.format(home, pdb_file), click={'fill': None, 'stroke': '#F0F0F0', 'size': 2.5, 'stroke-width': 10})
-
-# Logs the node data to the console when you click, in color_on_click() function!
-# console.log("Clicked Node Data:", d3.select(this).data()[0]);
+d3.show(filepath='{}/output/{}.html'.format(home, pdb_file), click={'fill': None, 'stroke': '#F0F0F0', 'size': 2.5, 'stroke-width': 10})
