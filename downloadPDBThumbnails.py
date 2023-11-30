df4fd401d39b6820b25b7cab773d88ecfe744b13
hiradhos <hiradhos@github.com>
2023-11-29 20:17:12 -0800

    Add png thumbnails

---
+++ downloadPDBThumbnails.py
@@ -0,0 +1,25 @@
+import requests
+import os
+
+
+def download_assembly_images(pdb_ids, output_folder="pdb_thumbnails"):
+    """
+    Downloads Assembly 1 images for a list of PDB IDs.
+    """
+
+    for pdb_id in pdb_ids:
+        image_url = f"https://cdn.rcsb.org/images/structures/{pdb_id}_assembly-1.jpeg"
+        response = requests.get(image_url)
+        if response.status_code == 200:
+            with open(os.path.join(output_folder, f"{pdb_id}_assembly1.png"), "wb") as file:
+                file.write(response.content)
+        else:
+            print(f"Failed to download image for {pdb_id}")
+
+def main():
+    with open("nakb_prna_ids.txt", "r") as file:
+        pdb_ids = file.read().split(",")
+    download_assembly_images(pdb_ids)
+
+if __name__ == "__main__":
+    main()
