052dd35745d91b07800aff071ddd7890b379a7a5
timkartar <timkartar7879@gmail.com>
2024-06-14 16:35:41 -0700

    download thumbnails in frontend

--- downloadPDBThumbnails.py
+++ downloadPDBThumbnails.py
@@ -2,7 +2,7 @@ import requests
 import os
 
 
-def download_assembly_images(pdb_ids, output_folder="pdb_thumbnails"):
+def download_assembly_images(pdb_ids, output_folder="../rnaprodb_frontend/public/pdb_thumbnails"):
     """
     Downloads Assembly 1 images for a list of PDB IDs.
     """
@@ -13,12 +13,14 @@ def download_assembly_images(pdb_ids, output_folder="pdb_thumbnails"):
         if response.status_code == 200:
             with open(os.path.join(output_folder, f"{pdb_id}_assembly1.png"), "wb") as file:
                 file.write(response.content)
+            os.chmod(os.path.join(output_folder, f"{pdb_id}_assembly1.png"), 777)
         else:
             print(f"Failed to download image for {pdb_id}")
 
 def main():
     with open("nakb_prna_ids.txt", "r") as file:
         pdb_ids = file.read().split(",")
+
     download_assembly_images(pdb_ids)
 
 if __name__ == "__main__":
