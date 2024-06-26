7d17e7a9a94b60b9ee990fac9684ea251f110575
timkartar <timkartar7879@gmail.com>
2024-06-26 10:10:58 -0700

    fallback for missing nt sec structure key

--- get_ss.py
+++ get_ss.py
@@ -42,47 +42,50 @@ def dssrSS(data):
         for item in data['nts']:
             id1 = dssr_id_to_text(item['nt_id'])
             dssrss[id1] = []
-    if 'stems' in data.keys():
-        for item in data['stems']:
-            stem_index = item['index']
-            helix_index = item['helix_index']
-            for pair in item['pairs']:
-                id1 = dssr_id_to_text(pair['nt1'])
-                id2 = dssr_id_to_text(pair['nt2'])
-                dssrss[id1].append(["stem", stem_index, helix_index])
-                dssrss[id2].append(["stem", stem_index, helix_index])
-    if 'hairpins' in data.keys():
-        for item in data['hairpins']:
-            index = item['index']
-            for nt in item['nts_long'].split(","):
-                id1 = dssr_id_to_text(nt)
-                dssrss[id1].append(["hairpins", index, item['type']])
-    if 'junctions' in data.keys():
-        for item in data['junctions']:
-            index = item['index']
-            for nt in item['nts_long'].split(","):
-                id1 = dssr_id_to_text(nt)
-                dssrss[id1].append(["junctions", index, item['type']])
-    if 'bulges' in data.keys():
-        for item in data['bulges']:
-            index = item['index']
-            for nt in item['nts_long'].split(","):
-                id1 = dssr_id_to_text(nt)
-                dssrss[id1].append(["bulges", index, item['type']])
-    
-    if 'iloops' in data.keys():
-        for item in data['iloops']:
-            index = item['index']
-            for nt in item['nts_long'].split(","):
-                id1 = dssr_id_to_text(nt)
-                dssrss[id1].append(["iloops", index, item['type']])
-    if 'ssSegments' in data.keys():
-        for item in data['ssSegments']:
-            index = item['index']
-            for nt in item['nts_long'].split(","):
-                id1 = dssr_id_to_text(nt)
-                dssrss[id1].append(["ssSegments", index, 'ssSegments'])
 
+    try:
+        if 'stems' in data.keys():
+            for item in data['stems']:
+                stem_index = item['index']
+                helix_index = item['helix_index']
+                for pair in item['pairs']:
+                    id1 = dssr_id_to_text(pair['nt1'])
+                    id2 = dssr_id_to_text(pair['nt2'])
+                    dssrss[id1].append(["stem", stem_index, helix_index])
+                    dssrss[id2].append(["stem", stem_index, helix_index])
+        if 'hairpins' in data.keys():
+            for item in data['hairpins']:
+                index = item['index']
+                for nt in item['nts_long'].split(","):
+                    id1 = dssr_id_to_text(nt)
+                    dssrss[id1].append(["hairpins", index, item['type']])
+        if 'junctions' in data.keys():
+            for item in data['junctions']:
+                index = item['index']
+                for nt in item['nts_long'].split(","):
+                    id1 = dssr_id_to_text(nt)
+                    dssrss[id1].append(["junctions", index, item['type']])
+        if 'bulges' in data.keys():
+            for item in data['bulges']:
+                index = item['index']
+                for nt in item['nts_long'].split(","):
+                    id1 = dssr_id_to_text(nt)
+                    dssrss[id1].append(["bulges", index, item['type']])
+        
+        if 'iloops' in data.keys():
+            for item in data['iloops']:
+                index = item['index']
+                for nt in item['nts_long'].split(","):
+                    id1 = dssr_id_to_text(nt)
+                    dssrss[id1].append(["iloops", index, item['type']])
+        if 'ssSegments' in data.keys():
+            for item in data['ssSegments']:
+                index = item['index']
+                for nt in item['nts_long'].split(","):
+                    id1 = dssr_id_to_text(nt)
+                    dssrss[id1].append(["ssSegments", index, 'ssSegments'])
+    except:
+        pass
     ''' Tertiary structure
     if 'Kturns' in data.keys():
         for item in data['kturns']:
