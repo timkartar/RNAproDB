8ecafb25418f0076b86d59611790c4304bc268e6
Ari Cohen <aricohen@localhost.local>
2024-07-03 13:25:59 -0700

    local server changes

--- run_rna_vis_server.sh
+++ run_rna_vis_server.sh
@@ -4,4 +4,10 @@
 PATH=$PATH:/srv/www/deeppbs.usc.edu/deeppbs-webserver/deeppbs/dependencies/bin
 PATH=$PATH:/srv/www/rnaprodb/rnaprodb_dev/external
 cd /srv/www/rnaprodb/rnaprodb_dev
-/home/aricohen/.conda/envs/rnascape/bin/python ./rna_vis.py $1 $2 $3 >output.log 2>error.log
+rm -f output.log
+rm -f $1-assembly1.pdb
+rm -f $1-assembly1.hb2
+rm -f error.log
+/home/aricohen/.conda/envs/rnascape/bin/python ./rna_vis.py $1 $2 $3
+# >output.log 2>error.log
+cd -
