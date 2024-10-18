270862fc14ff34a75d03f758ee168d48adc4b480
Ari Cohen <aricohen@yahoo.com>
2024-10-18 14:32:45 -0700

    fix dssr path for cifs and uploads

--- run_dssr.py
+++ run_dssr.py
@@ -12,7 +12,7 @@ from Bio.PDB import PDBIO, MMCIFIO, PDBParser, MMCIFParser
 parser= MMCIFParser()
 
 backend =  os.path.dirname(os.path.abspath(__file__))
-frontend = backend + "/../rnaprodb_frontend/"
+cifs = backend + "/output/cifs/"
 
 def runDSSR(structure, quiet=True, prefix='rna', tmpdir=''):
     """Run DSSR on given PDB file.
@@ -22,7 +22,7 @@ def runDSSR(structure, quiet=True, prefix='rna', tmpdir=''):
     prefix: string
         The file prefix.
     """
-    pdbpath = frontend + "/build/cifs/{}".format(tmpdir)
+    pdbpath = cifs + "/{}".format(tmpdir)
     outpath = backend + "/dssr_output/{}".format(tmpdir)
     if not os.path.exists(outpath):
         os.makedirs(outpath)
@@ -57,10 +57,10 @@ if __name__ == "__main__":
     
     #update: no need to change anymore
     # home =  os.path.dirname(os.path.abspath(__file__)) #change this line only
-    pdb_path = frontend + "/build/cifs/"
+    pdb_path = cifs
     # pdb_file = f"{pdb_id}-assembly1.cif"
     
-    for item in tqdm(open(frontend + "/build/cifs/{}-assembly1.cif".format(pdb_id),"r").readlines()):
+    for item in tqdm(open(cifs+"/{}-assembly1.cif".format(pdb_id),"r").readlines()):
         item = item.strip()
         #f = os.path.join(cifdir, fname)
         pdb_file = item #"{}-assembly1.cif".format(prefix)
