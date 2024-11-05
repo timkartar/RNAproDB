60913f67ea63cb2bcb79de049666d38c7273c6c0
Ari Cohen <aricohen@yahoo.com>
2024-11-05 14:31:02 -0800

    testing new views

--- backend/rnaprodb/urls.py
+++ backend/rnaprodb/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('get_struct_info/', views.get_struct_info, name='get_struct_info'),
     path('download_json/', views.download_json, name='download_json'),
     path('handle_upload', views.handle_upload, name='handle_upload'),
-    path('run-electrostatics', views.run_electrostatics, name='run_electrostatics')
+    path('run-electrostatics', views.run_electrostatics, name='run_electrostatics'),
+    path('get-struct-list', views.get_struct_list, name='get_struct_list'),
 ]
