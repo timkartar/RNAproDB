36118848dec496bc948581447a0f5893a75f8236
Ari Cohen <aricohen@yahoo.com>
2024-11-21 11:23:27 -0800

    remove RNA model unnecessary

--- backend/rnaprodb/admin.py
+++ backend/rnaprodb/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
-from .models import RNA
+# from .models import RNA
 
 class RnaProDbAdmin(admin.ModelAdmin):
     list_display = ('name', 'chain', 'position')
 # Register your models here.
 
-admin.site.register(RNA,RnaProDbAdmin)
+# admin.site.register(RNA,RnaProDbAdmin)
